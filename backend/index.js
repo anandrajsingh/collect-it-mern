bcb8ad76708db866eeea9691f6109a457a694df9
Anand Raj Singh <singhpariharanandraj@gmail.com>
2024-04-05 15:26:24 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
+require('dotenv').config();
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
+
+try {
+    const connect = mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => console.log("mongodb connected."))
+} catch (error) {
+    console.log(error)
+}
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World.')
+    res.json({ message: "Hello!" })
 })
 
 app.listen(port, () => {
